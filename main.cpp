6f36acff5f58aed66863c0a89c40d252f388a708
CinarBayramic <93824489+CinarBayramic@users.noreply.github.com>
2025-10-04 23:23:51 +0300

    add secant

--- main.cpp
+++ main.cpp
@@ -4,52 +4,72 @@
 #include <math.h>
 #include <iostream>
 #include <unordered_map>
-void helpfun(int argc,char**argv) {
+double helpfun(int argc,char**argv,bool printout) {
     std::cout << "Available Arguments:\n"
     "HELP -> Displays this menu\n"
-    "SIN <VAL(double)> -> Displays the sine of the second argument";
-    "COS <VAL(double)> -> Displays the cosine of the second argument";
-    "TAN <VAL(double)> -> Displays the tangent of the second argument";
-    "ATAN <VAL(double)> -> Displays the arc tangent of the second argument";
-    std::exit(0);
+    "SIN <VAL(double)> -> Displays the sine of the second argument\n"
+    "COS <VAL(double)> -> Displays the cosine of the second argument\n"
+    "TAN <VAL(double)> -> Displays the tangent of the second argument\n"
+    "ATAN <VAL(double)> -> Displays the arc tangent of the second argument\n"
+    "SEC <VAL(double)> -> Displays the secant of the second argument\n";
+    return 0;
 }
 //void sinfun(int argc,char**argv) {
 //    std::exit(0);
 //}
-void sinfun(int argc,char**argv) {
-    std::cout << sin(std::stod(argv[2]));
-    std::exit(0);
+double sinfun(int argc,char**argv,bool printout) {
+    double val = sin(std::stod(argv[2]));
+    if(printout) {
+    std::cout << val;
+    }
+    return val;
 }
-void cosfun(int argc,char**argv) {
-    std::cout << cos(std::stod(argv[2]));
-    std::exit(0);
+double cosfun(int argc,char**argv,bool printout) {
+  double val = cos(std::stod(argv[2]));
+  if(printout) {
+  std::cout << val;
+  }
+  return val;
 }
-void tanfun(int argc,char**argv) {
-    std::cout << tan(std::stod(argv[2]));
-    std::exit(0);
+double tanfun(int argc,char**argv,bool printout) {
+  double val = tan(std::stod(argv[2]));
+  if(printout) {
+  std::cout << val;
+  }
+  return val;
 }
-void arctanfun(int argc,char**argv) {
+double arctanfun(int argc,char**argv,bool printout) {
     double x = (std::stod(argv[2]));
     double sum = 0;
     for(double n = 0; n < x; n+=dn) {
       sum += (1/(1+n*n)) * dn;
     }
+    if(printout) {
     std::cout << sum;
-    std::exit(0);
+    }
+    return sum;
+}
+double secfun(int argc,char**argv,bool printout) {
+  double val = 1/cosfun(argc,argv,false);  
+  if(printout) {
+  std::cout << val;
+  }
+  return val;
 }
 int main(int argc,char**argv) {
   if(argc < 2) {
     std::cout << "at least a single argument needs to be specified\nFor more information run this program with the \"HELP\" command."<< argc  <<"\n";
     return 0;
   }
-  std::unordered_map<std::string, void(*)(int argc,char**argv)> cmdfun = {
+  std::unordered_map<std::string, double(*)(int argc,char**argv,bool printout)> cmdfun = {
     {"HELP",helpfun},
     {"SIN",sinfun},
     {"COS",cosfun},
     {"TAN",tanfun},
     {"ATAN",arctanfun},
+    {"SEC",secfun}
   };
-  cmdfun[argv[1]](argc,argv);
+  cmdfun[argv[1]](argc,argv,true);
 
 
   return 0;
